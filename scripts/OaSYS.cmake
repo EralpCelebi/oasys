915186478693f6e2d22d3071e83dceff769ff599
Eralp Çelebi <eralp.celebi.personal@gmail.com>
2024-04-26 16:12:07 +0300

    refactor: Moved option definitions.
    Moved option definitions from the root CMakeLists.txt into the scripts folder for consistency.

--- scripts/OaSYS.cmake
+++ scripts/OaSYS.cmake
@@ -1,1 +1,7 @@
+INCLUDE(CMakeDependentOption)
+
+OPTION(CONFIG_BOOT "Boot: Builds and packages a bootloader alongside the kernel." ON) 
+CMAKE_DEPENDENT_OPTION(CONFIG_BOOT_EFI "Boot: Builds EFI-capable image." ON "CONFIG_BOOT" OFF)
+CMAKE_DEPENDENT_OPTION(CONFIG_BOOT_EFI_PACK "Boot: Creates EFI-capable boot media." ON "CONFIG_BOOT_EFI" OFF)
+
 INCLUDE(${SCRIPTS}/OaSYS.BOOT.cmake)
