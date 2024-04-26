bda5559eaf2eb9c7fc3b041a76fc251523cd7639
Eralp Çelebi <eralp.celebi.personal@gmail.com>
2024-04-26 19:02:04 +0300

    feat: Added CONFIG_BOOT_GREET.
    Added an option to greet the user for the bootloader.

--- boot/boot.c
+++ boot/boot.c
@@ -1,5 +1,6 @@
 #include <efi.h>
 #include <efilib.h>
+#include <oasys/oasys.h>
 
 EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
     EFI_STATUS    Status;
@@ -7,11 +8,27 @@ EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE* SystemTable) {
 
     ST = SystemTable;
 
-    Status = ST->ConOut->OutputString(ST->ConOut, L"Hello World\r\n");
+    Status = ST->ConOut->OutputString(ST->ConOut, L"OaSYS.Kernel\r\n");
     if (EFI_ERROR(Status)) return Status;
 
     Status = ST->ConIn->Reset(ST->ConIn, FALSE);
     if (EFI_ERROR(Status)) return Status;
+
+#ifdef CONFIG_BOOT_GREET
+    CHAR16* Messages[] = { L"Architecture: ", L"Toolchain: ", L"Version: " };
+    CHAR16* Outputs[]  = { CONFIG_ARCH L"\r\n", CONFIG_TOOLCHAIN L"\r\n", OASYS_VERSION L"\r\n" };
+
+    for (uint8_t Index; Index < 3; Index++) {
+        Status = ST->ConOut->OutputString(ST->ConOut, Messages[Index]);
+        if (EFI_ERROR(Status)) return Status;
+
+        Status = ST->ConOut->OutputString(ST->ConOut, Outputs[Index]);
+        if (EFI_ERROR(Status)) return Status;
+
+        Status = ST->ConIn->Reset(ST->ConIn, FALSE);
+        if (EFI_ERROR(Status)) return Status;
+    }
+#endif
 
     while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) {}
 
--- scripts/OaSYS.cmake
+++ scripts/OaSYS.cmake
@@ -1,12 +1,13 @@
 INCLUDE(CMakeDependentOption)
 
-SET(CONFIG_ARCH "x64" CACHE STRING "Build: Targets specified architecture.")
+SET(CONFIG_ARCH "x64" CACHE STRING "Build: Target specified architecture.")
 SET_PROPERTY(CACHE CONFIG_ARCH PROPERTY STRINGS "x64;aa64")
 
-SET(CONFIG_TOOLCHAIN "llvm" CACHE STRING "Build: Selects active toolchain.")
+SET(CONFIG_TOOLCHAIN "llvm" CACHE STRING "Build: Select active toolchain.")
 SET_PROPERTY(CACHE CONFIG_TOOLCHAIN PROPERTY STRINGS "gnu;llvm")
 
 OPTION(CONFIG_BOOT "Boot: Builds an EFI-capable bootloader alongside the kernel." ON)
-CMAKE_DEPENDENT_OPTION(CONFIG_BOOT_PACK "Boot: Creates EFI-capable boot media." ON "CONFIG_BOOT" OFF)
+CMAKE_DEPENDENT_OPTION(CONFIG_BOOT_PACK "Boot: Create EFI-capable boot media." ON "CONFIG_BOOT" OFF)
+CMAKE_DEPENDENT_OPTION(CONFIG_BOOT_GREET "Boot: Greet user." ON "CONFIG_BOOT" OFF)
 
 INCLUDE(${SCRIPTS}/OaSYS.BOOT.cmake)
