58859da5f348ca24999435164a107f98e36d6d1d
Eralp Çelebi <eralp.celebi.personal@gmail.com>
2024-05-02 17:01:55 +0300

    refactor: Fixed inconsistencies.
    Fixes certain out-of-style files. Reworks header generation.

--- scripts/OaSYS.cmake
+++ scripts/OaSYS.cmake
@@ -1,10 +1,14 @@
 INCLUDE(CMakeDependentOption)
 
+# Multi-state options.
+
 SET(CONFIG_ARCH "x64" CACHE STRING "Build: Target specified architecture.")
 SET_PROPERTY(CACHE CONFIG_ARCH PROPERTY STRINGS "x64;aa64")
 
 SET(CONFIG_TOOLCHAIN "llvm" CACHE STRING "Build: Select active toolchain.")
 SET_PROPERTY(CACHE CONFIG_TOOLCHAIN PROPERTY STRINGS "gnu;llvm")
+
+# Conventional options.
 
 OPTION(CONFIG_BOOT "Boot: Builds an EFI-capable bootloader alongside the kernel." ON)
 CMAKE_DEPENDENT_OPTION(CONFIG_BOOT_PACK "Boot: Create EFI-capable boot media." ON "CONFIG_BOOT" OFF)
